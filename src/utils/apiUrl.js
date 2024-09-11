5e7bb31ff67779df381ec29740716b5d57234762
mjk2024-dementia <minjukang20@gmail.com>
2024-09-11 10:40:12 +0900

    first commit

---
+++ src/utils/apiUrl.js
@@ -0,0 +1,14 @@
+const GET_TASKS_API_URL = 'https://taskback.jinwoo1.store/get_tasks';
+const POST_TASKS_API_URL = 'https://taskback.jinwoo1.store/post_task';
+const DELETE_TASKS_API_URL = 'https://taskback.jinwoo1.store/delete_task';
+const UPDATE_COMPLETED_TASKS_API_URL =
+  'https://taskback.jinwoo1.store/update_completed_task';
+const UPDATE_TASK_API_URL = 'https://taskback.jinwoo1.store/update_task';
+
+export {
+  GET_TASKS_API_URL,
+  POST_TASKS_API_URL,
+  DELETE_TASKS_API_URL,
+  UPDATE_COMPLETED_TASKS_API_URL,
+  UPDATE_TASK_API_URL,
+};
